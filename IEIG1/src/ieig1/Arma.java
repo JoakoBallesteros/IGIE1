e6afcc80b94944da5144a6cd6187d4d680f58987
FedeTerradas <federico.terradas04@gmail.com>
2025-09-27 15:21:17 -0300

    Solucionado Error clases duplicadas

--- IEIG1/src/ieig1/Arma.java
+++ IEIG1/src/ieig1/Arma.java
@@ -23,30 +23,4 @@ abstract class Arma {
     public abstract void usarEfectoEspecial(Personaje portador, Personaje enemigo);
 }
 
-// ------------------- ARMAS HEROE -------------------
-class EspadaSimple extends Arma {
-    public EspadaSimple() { super("Espada Simple", 5); }
-    @Override
-    public void usarEfectoEspecial(Personaje portador, Personaje enemigo) {
-        System.out.println("La espada simple no tiene efectos especiales.");
-    }
-}
 
-class EspadaSagrada extends Arma {
-    public EspadaSagrada() { super("Espada Sagrada", 10); }
-    @Override
-    public void usarEfectoEspecial(Personaje portador, Personaje enemigo) {
-        System.out.println(portador.nombre + " se cura 10 de vida gracias a la espada sagrada.");
-        portador.vida += 10;
-    }
-}
-
-class EspadaCelestial extends Arma {
-    public EspadaCelestial() { super("Espada Celestial", 15); }
-    @Override
-    public void usarEfectoEspecial(Personaje portador, Personaje enemigo) {
-        System.out.println(portador.nombre + " se cura 20 de vida y gana +5 defensa.");
-        portador.vida += 20;
-        portador.defensa += 5;
-    }
-}
