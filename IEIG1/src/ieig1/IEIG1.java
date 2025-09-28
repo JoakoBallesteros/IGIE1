698d087f2c0c3e8d46f54c81819a5a0d33018c52
FedeTerradas <federico.terradas04@gmail.com>
2025-09-28 17:54:18 -0300

    Cambios en las stats, agregada la pausa

--- IEIG1/src/ieig1/IEIG1.java
+++ IEIG1/src/ieig1/IEIG1.java
@@ -8,9 +8,9 @@ package ieig1;
  *
  * @author Fede
  */
-
 import java.util.Random;
 import java.util.Scanner;
+
 public class IEIG1 {
 
     /**
@@ -23,19 +23,27 @@ public class IEIG1 {
         // Crear héroe
         System.out.print("Ingrese nombre del héroe: ");
         String nombreHeroe = sc.nextLine();
-        Heroe heroe = new Heroe(nombreHeroe, 100, 20, 5, rnd.nextInt(101));
+        Heroe heroe = new Heroe(nombreHeroe,
+                100 + rnd.nextInt(41),
+                21 + rnd.nextInt(11),
+                5 + rnd.nextInt(8),
+                rnd.nextInt(102));
 
         // Crear villano
         System.out.print("Ingrese nombre del villano: ");
         String nombreVillano = sc.nextLine();
-        Villano villano = new Villano(nombreVillano, 100, 20, 5, rnd.nextInt(101));
+        Villano villano = new Villano(nombreVillano,
+                90 + rnd.nextInt(41),
+                20 + rnd.nextInt(11),
+                6 + rnd.nextInt(8),
+                rnd.nextInt(101));
 
-        // Mostrar estado inicial
         System.out.println("\n⚔️ ¡Comienza la batalla! ⚔️");
+        pausa(1500);
         System.out.println(heroe);
         System.out.println(villano);
         System.out.println();
-
+        pausa(2000);
 
         if (heroe.bendicion >= 100) {
             System.out.println("Heroe " + heroe.nombre + " puede usar Castigo Bendito!");
@@ -44,17 +52,20 @@ public class IEIG1 {
             System.out.println(" Villano " + villano.nombre + " puede usar Leviatán del Vacío!");
         }
         System.out.println();
+        pausa(1000);
 
-
-        // Simulación de batalla
         while (heroe.estaVivo() && villano.estaVivo()) {
             heroe.decidirAccion(villano);
-            if (!villano.estaVivo()) break;
+            pausa(1500);
+            if (!villano.estaVivo()) {
+                break;
+            }
             villano.decidirAccion(heroe);
+            pausa(1500);
             System.out.println();
         }
 
-        // Resultado
+        pausa(1000);
         if (heroe.estaVivo()) {
             System.out.println(heroe.nombre + " ha ganado la batalla!");
         } else {
@@ -63,5 +74,12 @@ public class IEIG1 {
 
         sc.close();
     }
-    
+
+    public static void pausa(int milisegundos) {
+        try {
+            Thread.sleep(milisegundos);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
 }
