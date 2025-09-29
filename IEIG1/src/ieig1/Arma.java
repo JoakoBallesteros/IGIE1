b99d2d7f1179dbf1317f8d1df05e77e09427018c
fedem <fedem@host.docker.internal>
2025-09-29 02:32:22 -0300

    Modificado por Lucas Barberis y Federico medina: Reporte final de la batalla

--- IEIG1/src/ieig1/Arma.java
+++ IEIG1/src/ieig1/Arma.java
@@ -1,6 +1,9 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package ieig1;
 
@@ -22,5 +25,3 @@ abstract class Arma {
 
     public abstract void usarEfectoEspecial(Personaje portador, Personaje enemigo);
 }
-
-
